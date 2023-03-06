834ee769bb9aa4630d5125aea383279921fd60f6
Cameron Morgan <cameron.morgan@skiff.com>
2023-03-05 17:43:12 -0800

    Completed Module 3 - Objective 2 tasks.

--- form-demo-1/src/App.js
+++ form-demo-1/src/App.js
@@ -1,36 +1,67 @@
+import { useState, useEffect } from 'react';
+import * as yup from 'yup';
 import './App.css';
-import { useState } from 'react';
+
+// yup schema - normally stored in a separate file
+const schema = yup.object().shape({
+  user: yup.string().required('user is required').min(6, 'user needs to be a minimum of 6 chars'),
+  star: yup.string().oneOf(["wars", "trek"], "you must select a star"),
+  agree: yup.boolean().oneOf([true], "you must give away all of your data"),
+  language: yup.string().oneOf(["1", "2", "3"], "you must choose a language")
+})
 
 function App() {
 
-  const [form, setForm] = useState({
-    user: "",
-    star: "",
-    agree: false,
-    language: ""
-  });
+  const [form, setForm] = useState({ user: "", star: "", agree: false, language: "" });
+  const [disabled, setDisabled] = useState(true);
+  const [errors, setErrors] = useState({ user: "", star: "", agree: "", language: "" })
+  const setFormErrors = (name, value) => {
+    
+    // asynchronous promise, so use a "then()"!
+    yup.reach(schema, name).validate(value)
+      .then(() => {
+        setErrors({ ...errors, [name]: '' })
+      })
+      .catch(err => {
+        setErrors({ ...errors, [name]: err.errors[0] })
+      })
+  }
 
   // callback function takes an event object
   const change = event => {
     const { checked, value, name, type } = event.target;
     // if checkbox type, then use "checked" from event.target, else use "value" from event.target
     const valueToUse = type === "checkbox" ? checked : value;
+    setFormErrors(name, valueToUse);
     setForm({ ...form, [name]: valueToUse});
   }
 
+  useEffect(() => {
+    // if the schema is valid...
+    schema.isValid(form)
+      .then(valid => setDisabled(!valid))
+      .catch(err => console.error(err))
+  }, [form])
+
   return (
     <div className="App">
+      <div style={{ color: "red" }}>
+        <div>{errors.user}</div>
+        <div>{errors.star}</div>
+        <div>{errors.agree}</div>
+        <div>{errors.language}</div>
+      </div>
       <form>
         <label>User&nbsp;
           <input name="user" type="text" value={form.user} onChange={change} />
         </label>
 
         <label>Star Trek
-          <input name="star" type="radio" value="trek" checked={form.start === "check"} onChange={change}  />
+          <input name="star" type="radio" value="trek" checked={form.star === "trek"} onChange={change}  />
         </label>
 
         <label>Star Wars
-          <input name="star" type="radio" value="wars" checked={form.start === "wars"} onChange={change}  />
+          <input name="star" type="radio" value="wars" checked={form.star === "wars"} onChange={change}  />
         </label>
 
         <label>Give away your data&nbsp;
@@ -45,8 +76,7 @@ function App() {
             <option value="3">Java</option>
           </select>
         </label>
-
-
+        <button disabled={disabled}>Submit</button>
       </form>
     </div>
   );
